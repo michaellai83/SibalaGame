b6192859a5c2575b6fff5a9470693a1657418715
michaellai83 <michael.lai@PC-MICHAELLAI>
2024-08-30 10:14:50 +0800

    (Refactor)移至test資料夾底下

--- test/SibalaGameTest.cs
+++ test/SibalaGameTest.cs
@@ -1,19 +1,23 @@
 using FluentAssertions;
+using SibalaGame_20240830.@class;
 
-namespace SibalaGame_20240830
+namespace SibalaGame_20240830.test
 {
     public class SibalaGameTest
     {
+        private SiblaGame _game;
+
         [SetUp]
         public void Setup()
         {
+            _game = new SiblaGame();
         }
 
         [Test]
         public void A01_Both_AllOfAKind()
         {
-            var game = new SiblaGame();
-            var actual = game.ShowResult("Black: 5 5 5 5  White: 2 2 2 2");
+            
+            var actual = _game.ShowResult("Black: 5 5 5 5  White: 2 2 2 2");
             actual.Should().Be("Black win. - with all of a kind: 5");
         }
     }
