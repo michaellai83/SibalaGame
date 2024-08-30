835c6c6fe27aa5d8fb98e97ca3ab5c9e750e8562
michaellai83 <michael.lai@PC-MICHAELLAI>
2024-08-30 12:09:36 +0800

    (Pass test case)A02_Both_AllOfAKind_White_Win

--- Model/SiblaGame.cs
+++ Model/SiblaGame.cs
@@ -11,11 +11,23 @@
             var parser = new Parser();
             var players = parser.Parse(input);
 
+            var player1 = players[0];
+            var player2 = players[1];
+
+            var player1DicesPoint = player1.Dices[0].Value;
+            var player2DicesPoint = player2.Dices[0].Value;
 
             var winnerName = players[0].Name;
             var winnerCategory = players[0].Category;
             var winnerOutput = players[0].Dices[0].Output;
 
+            if (player2DicesPoint >  player1DicesPoint)
+            {
+                winnerName = player2.Name;
+                winnerCategory = player2.Category;
+                winnerOutput = player2.Dices[0].Output;
+            }
+            
             return $"{winnerName} win. - with {winnerCategory}: {winnerOutput}";
         }
     }
--- test/SibalaGameTest.cs
+++ test/SibalaGameTest.cs
@@ -14,11 +14,17 @@ namespace SibalaGame_20240830.test
         }
 
         [Test]
-        public void A01_Both_AllOfAKind()
-        {
-            
+        public void A01_Both_AllOfAKind_Black_Win()
+        {        
             var actual = _game.ShowResult("Black: 5 5 5 5  White: 2 2 2 2");
             actual.Should().Be("Black win. - with all of a kind: 5");
+        }
+
+        [Test]
+        public void A02_Both_AllOfAKind_White_Win()
+        {
+            var actual = _game.ShowResult("Black: 3 3 3 3  White: 6 6 6 6");
+            actual.Should().Be("White win. - with all of a kind: 6");
         }
     }
 }
