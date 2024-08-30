e850a26e3c8e9efbb3e5ad74848909dd2a668d1e
michaellai83 <michael.lai@PC-MICHAELLAI>
2024-08-30 10:01:35 +0800

    加入專案檔案。

---
+++ SibalaGameTest.cs
@@ -0,0 +1,16 @@
+namespace SibalaGame_20240830
+{
+    public class SibalaGameTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
