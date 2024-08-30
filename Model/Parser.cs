2dae8d5102338b7645fe94a903f4d1e6366104f4
michaellai83 <michael.lai@PC-MICHAELLAI>
2024-08-30 11:41:55 +0800

    (Refactor)Parser:Modify Parse method

--- Model/Parser.cs
+++ Model/Parser.cs
@@ -9,42 +9,36 @@
         public List<Player> Parse(string input)
         {
             var playerSections = input.Split("  ", StringSplitOptions.RemoveEmptyEntries);
-            var player1Name = GetPlayerName(playerSections, 0);
-            var player1Dices = GetPlayerDices(playerSections, 0);
+            var result = new List<Player>();
 
-            var player2Name = GetPlayerName(playerSections, 1);
-            var player2Dices = GetPlayerDices(playerSections, 1);
-
-            return new List<Player>
+            foreach (var playerSection in playerSections)
+            {
+                var player = new Player
                 {
-                    new Player
-                    {
-                        Name = player1Name,
-                        Dices = player1Dices,
-                    },
-                    new Player
-                    {
-                        Name = player2Name,
-                        Dices = player2Dices,
-                    }
+                    Name = GetPlayerName(playerSection),
+                    Dices = GetPlayerDices(playerSection),
                 };
+                result.Add(player);
+            }
+
+            return result;
         }
 
-        private static List<Dice> GetPlayerDices(string[] playerSections, int playerIndex)
+        private static List<Dice> GetPlayerDices(string playerSection)
         {
-            return playerSections[playerIndex]
-                            .Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
-                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                            .Select(s => new Dice
-                            {
-                                Value = int.Parse(s),
-                                Output = s
-                            }).ToList();
+            return playerSection
+                .Split(":", StringSplitOptions.RemoveEmptyEntries)[1]
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => new Dice
+                {
+                    Value = int.Parse(s),
+                    Output = s
+                }).ToList();
         }
 
-        private static string GetPlayerName(string[] playerSections, int playerIndex)
+        private static string GetPlayerName(string playerSection)
         {
-            return playerSections[playerIndex].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+            return playerSection.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
         }
     }
 }
