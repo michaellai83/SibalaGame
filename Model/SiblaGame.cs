ec8376befe555d8562d6d25213c0ec4ae825598c
michaellai83 <michael.lai@PC-MICHAELLAI>
2024-08-30 15:28:30 +0800

    (Refactor)SiblaGame: Extract DetermineWinner,IsAllOfAKind,CompareAllofAKind,CompareSumAndHighestDice method

--- Model/SiblaGame.cs
+++ Model/SiblaGame.cs
@@ -23,44 +23,62 @@
             var player1 = players[0];
             var player2 = players[1];
 
-            var winner = player1;
+            var winner = DetermineWinner(player1, player2);
 
-            if (player1.Category == "all of a kind" || player2.Category == "all of a kind")
+            if (winner == null)
             {
-                if (player1.Dices[0].Value == player2.Dices[0].Value)
-                {
-                    return "Tie.";
-                }
+                return "Tie.";
+            }
 
-                if (_AllOfAKindLookup[player2.Dices[0].Value] > _AllOfAKindLookup[player1.Dices[0].Value])
-                {
-                    winner = player2;
-                }
-
+            if (IsAllOfAKind(winner))
+            {
                 return $"{winner.Name} win. - with {winner.Category}: {winner.Dices[0].Output}";
             }
 
-            var player1Sum = player1.Dices[2].Value + player1.Dices[3].Value;
-            var player2Sum = player2.Dices[2].Value + player2.Dices[3].Value;
-            
-            if(player1Sum == player2Sum)
-            {
-                if (player2.Dices[3].Value > player1.Dices[3].Value)
-                {
-                    winner = player2;
-                }
-                else
-                {
-                    return "Tie.";
-                }                
-            }
-
-            if (player2Sum > player1Sum)
-            {
-                winner = player2;
-            }
 
             return $"{winner.Name} win. - with {winner.Category}: {winner.Dices[2].Value + winner.Dices[3].Value}";
+        }
+
+        private Player DetermineWinner(Player player1, Player player2)
+        {
+            if (IsAllOfAKind(player1) || IsAllOfAKind(player2))
+            {
+                return CompareAllOfAKind(player1, player2);
+            }
+
+            return CompareSumAndHighestDice(player1, player2);
+        }
+
+        private bool IsAllOfAKind(Player player) => player.Category == "all of a kind";
+
+        private Player CompareAllOfAKind(Player player1, Player player2)
+        {
+            if (player1.Category == player2.Category)
+            {
+                if (player1.Dices[0].Value == player2.Dices[0].Value)
+                {
+                    return null; // Tie
+                }
+
+                return _AllOfAKindLookup[player2.Dices[0].Value] > _AllOfAKindLookup[player1.Dices[0].Value]
+                    ? player2
+                    : player1;
+            }
+
+            return player2.Category == "all of a kind" ? player2 : player1;
+        }
+
+        private Player CompareSumAndHighestDice(Player player1, Player player2)
+        {
+            var player1Sum = player1.Dices[2].Value + player1.Dices[3].Value;
+            var player2Sum = player2.Dices[2].Value + player2.Dices[3].Value;
+
+            if (player1Sum == player2Sum)
+            {
+                return player2.Dices[3].Value > player1.Dices[3].Value ? player2 : null; // Tie if player1.Dices[3].Value >= player2.Dices[3].Value
+            }
+
+            return player2Sum > player1Sum ? player2 : player1;
         }
     }
 }
